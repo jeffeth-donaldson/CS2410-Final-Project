b42cb6ddae3837feb011df678599b2090d3b7af5
Re8Hoo <re8hoopes@aggiemail.usu.edu>
2021-04-21 15:33:06 -0600

    Added task and new task fragements. Added Task, User, Schedule classes. Set up new task fragment

--- app/src/main/java/com/jeffethdonaldson/cs2410_final_project/MainActivity.java
+++ app/src/main/java/com/jeffethdonaldson/cs2410_final_project/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jeffethdonaldson.cs2410_final_project.fragments.TaskFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container_view, TaskFragment.class, null)
+                    .setReorderingAllowed(true)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/jeffethdonaldson/cs2410_final_project/models/Task.java
@@ -0,0 +1,26 @@
+package com.jeffethdonaldson.cs2410_final_project.models;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Task {
+    @PrimaryKey(autoGenerate = true)
+    public long id;
+
+    @ColumnInfo(name = "title")
+    public String name;
+
+    @ColumnInfo(name = "description")
+    public String description;
+    //daily, weekly, biweekly, monthly, yearly
+
+    @ColumnInfo(name = "frequency")
+    public String frequency;
+
+//    @ColumnInfo(name = "user")
+//    public User user;
+
+
+}
