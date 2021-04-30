30c54aa510578057afb9c5c19dba86b00a027b88
Joshua <josh.mcclung66@gmail.com>
2021-04-29 21:03:48 -0600

    Added TaskByRoomViewModel, and finished RoomFragment

--- app/src/main/java/com/jeffethdonaldson/cs2410_final_project/fragments/RoomFragment.java
+++ app/src/main/java/com/jeffethdonaldson/cs2410_final_project/fragments/RoomFragment.java
@@ -5,22 +5,29 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.ObservableArrayList;
+import androidx.databinding.ObservableList;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.jeffethdonaldson.cs2410_final_project.R;
 import com.jeffethdonaldson.cs2410_final_project.fragments.adapters.TaskAdapter;
 import com.jeffethdonaldson.cs2410_final_project.models.HouseRoom;
+import com.jeffethdonaldson.cs2410_final_project.models.Task;
 import com.jeffethdonaldson.cs2410_final_project.viewmodels.RoomViewModel;
+import com.jeffethdonaldson.cs2410_final_project.viewmodels.TaskByRoomViewModel;
 import com.jeffethdonaldson.cs2410_final_project.viewmodels.TaskViewModel;
 
 public class RoomFragment extends Fragment {
     public RoomFragment() {
         super(R.layout.fragment_room);
     }
-    TaskViewModel taskViewModel;
+    TaskByRoomViewModel taskViewModel;
     RoomViewModel roomViewModel;
     private HouseRoom currentRoom;
+    private ObservableArrayList<Task> tasks;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -36,17 +43,75 @@ public class RoomFragment extends Fragment {
             }
         });
 
-        taskViewModel = new ViewModelProvider(getActivity()).get(TaskViewModel.class);
-        //TaskAdapter adapter = new TaskAdapter(//taskViewModel.get)
-        //TODO: Finish this implementation
 
 
-        view.findViewById(R.id.fab_house).setOnClickListener((button) -> {
+        taskViewModel = new ViewModelProvider(getActivity()).get(TaskByRoomViewModel.class);
+
+        tasks = taskViewModel.getTasks(currentRoom);
+
+        TaskAdapter adapter = new TaskAdapter(
+                tasks,
+                (task) -> {
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container_view, AddTaskFragment.class, null)
+                            .setReorderingAllowed(true)
+                            .addToBackStack(null)
+                            .commit();
+                },
+                (task)->{
+                    //viewModel.delete(task);
+                }
+        );
+
+        tasks.addOnListChangedCallback(new ObservableList.OnListChangedCallback<ObservableList<Task>>() {
+            @Override
+            public void onChanged(ObservableList<Task> sender) {
+                getActivity().runOnUiThread(() -> {
+                    adapter.notifyDataSetChanged();
+                });
+            }
+
+            @Override
+            public void onItemRangeChanged(ObservableList<Task> sender, int positionStart, int itemCount) {
+                getActivity().runOnUiThread(() -> {
+                    adapter.notifyItemRangeChanged(positionStart, itemCount);
+                });
+            }
+
+            @Override
+            public void onItemRangeInserted(ObservableList<Task> sender, int positionStart, int itemCount) {
+                getActivity().runOnUiThread(() -> {
+                    adapter.notifyItemRangeInserted(positionStart, itemCount);
+                });
+            }
+
+            @Override
+            public void onItemRangeMoved(ObservableList<Task> sender, int fromPosition, int toPosition, int itemCount) {
+                getActivity().runOnUiThread(() -> {
+                    for (int i = 0; i < itemCount; i++) {
+                        adapter.notifyItemMoved(fromPosition+i, toPosition+i);
+                    }
+                });
+            }
+
+            @Override
+            public void onItemRangeRemoved(ObservableList<Task> sender, int positionStart, int itemCount) {
+                getActivity().runOnUiThread(() -> {
+                    adapter.notifyItemRangeRemoved(positionStart, itemCount);
+                });
+            }
+        });
+
+        view.findViewById(R.id.fab_room).setOnClickListener((button) -> {
             getActivity().getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container_view, AddTaskFragment.class, null)
                     .setReorderingAllowed(true)
                     .addToBackStack(null)
                     .commit();
         });
+
+        RecyclerView recyclerView = view.findViewById(R.id.room_fragment_recycler);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
---
+++ app/src/main/java/com/jeffethdonaldson/cs2410_final_project/viewmodels/TaskByRoomViewModel.java
@@ -0,0 +1,55 @@
+package com.jeffethdonaldson.cs2410_final_project.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.ObservableArrayList;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+import androidx.room.Room;
+
+import com.jeffethdonaldson.cs2410_final_project.R;
+import com.jeffethdonaldson.cs2410_final_project.db.AppDB;
+import com.jeffethdonaldson.cs2410_final_project.models.HouseRoom;
+import com.jeffethdonaldson.cs2410_final_project.models.Task;
+
+public class TaskByRoomViewModel extends AndroidViewModel {
+    ObservableArrayList<Task> tasks = new ObservableArrayList<>();
+    MutableLiveData<Boolean> saving = new MutableLiveData<>();
+    MutableLiveData<Task> currentTask = new MutableLiveData<>();
+    AppDB db;
+
+    public TaskByRoomViewModel(@NonNull Application application, HouseRoom houseRoom) {
+        super(application);
+        saving.setValue(false);
+        db = Room.databaseBuilder(application, AppDB.class,  application.getString(R.string.db_name)).build();
+    }
+
+    public ObservableArrayList<Task> getTasks(HouseRoom houseRoom){
+        new Thread(() -> {
+            tasks.addAll(db.getTaskDao().findByRoom(houseRoom.name));
+        }).start();
+        return tasks;
+    }
+    public MutableLiveData<Boolean> getSaving(){
+        return saving;
+    }
+    public MutableLiveData<Task> getCurrentTask(){
+        return currentTask;
+    }
+    public void setCurrentTask(Task currentTask){
+        this.currentTask.postValue(currentTask);
+    }
+    public void saveTask(String title, String description, int frequency, String userName, String roomName){
+        new Thread(()->{
+            Task newTask = new Task();
+            newTask.name = title;
+            newTask.description = description;
+            newTask.frequency = frequency;
+            newTask.user = userName;
+            newTask.room = roomName;
+            newTask.id = db.getTaskDao().insert(newTask);
+            tasks.add(newTask);
+        }).start();
+    }
+}
