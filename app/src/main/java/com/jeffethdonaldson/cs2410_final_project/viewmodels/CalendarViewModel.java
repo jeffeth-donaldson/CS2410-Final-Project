7591a943bd713f5b41c41b3391145c88a4a7099c
Joshua <josh.mcclung66@gmail.com>
2021-04-30 20:38:26 -0600

    Added Dependencies for sectioned recyclerview https://github.com/crux-lab/sectioned-recycler-view

---
+++ app/src/main/java/com/jeffethdonaldson/cs2410_final_project/viewmodels/CalendarViewModel.java
@@ -0,0 +1,10 @@
+package com.jeffethdonaldson.cs2410_final_project.viewmodels;
+
+import androidx.databinding.ObservableArrayList;
+import androidx.lifecycle.ViewModel;
+
+import com.jeffethdonaldson.cs2410_final_project.models.Day;
+
+public class CalendarViewModel extends ViewModel {
+
+}
